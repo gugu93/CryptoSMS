b51ea29a57f2454cade1883a25272323b540a650
Adrian <adex333@gmail.com>
2015-02-03 12:57:56 +0100

    Zrobienie szyfrowania

--- app/src/main/java/com/example/adrian/cryptosms/MassageActivity.java
+++ app/src/main/java/com/example/adrian/cryptosms/MassageActivity.java
@@ -4,14 +4,100 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.util.Base64;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 
 public class MassageActivity extends ActionBarActivity {
 
+    private EditText messageText;
+    private Button cancelButton;
+    private Button sendButton;
+    private Button encrypteButton;
+    private Button decryptButton;
+
+    public static final String DEFAULT_ENCODING="UTF-8";
+    //static BASE64Encoder enc=new BASE64Encoder();
+    //static BASE64Decoder dec=new BASE64Decoder();
+
+    public static String base64encode(String text){
+        try {
+            //String rez = enc.encode( text.getBytes( DEFAULT_ENCODING ) );
+            String rez = Base64.encodeToString(text.getBytes(DEFAULT_ENCODING),Base64.DEFAULT);
+            return rez;
+        }
+        catch ( UnsupportedEncodingException e ) {
+            return null;
+        }
+    }//base64encode
+
+    public static String base64decode(String text){
+
+        try {
+            //return new String(dec.decodeBuffer( text ),DEFAULT_ENCODING);
+            byte[] dec = Base64.decode(text,Base64.DEFAULT);
+            return new String(dec,"UTF-8");
+        }
+        catch ( IOException e ) {
+            return null;
+        }
+
+    }//base64decode
+
+//    public static void main(String[] args){
+//        String txt="some text to be encrypted" ;
+//        String key="key phrase used for XOR-ing";
+//        System.out.println(txt+" XOR-ed to: "+(txt=xorMessage( txt, key )));
+//        String encoded=base64encode( txt );
+//        System.out.println( " is encoded to: "+encoded+" and that is decoding to: "+ (txt=base64decode( encoded )));
+//        System.out.print( "XOR-ing back to original: "+xorMessage( txt, key ) );
+//
+//    }
+
+    public static String xorMessage(String message, String key){
+        try {
+            if (message==null || key==null ) return null;
+
+            char[] keys=key.toCharArray();
+            char[] mesg=message.toCharArray();
+
+            int ml=mesg.length;
+            int kl=keys.length;
+            char[] newmsg=new char[ml];
+
+            for (int i=0; i<ml; i++){
+                newmsg[i]=(char)(mesg[i]^keys[i%kl]);
+            }//for i
+            mesg=null; keys=null;
+            return new String(newmsg);
+        }
+        catch ( Exception e ) {
+            return null;
+        }
+    }//xorMessage
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_massage);
+        messageText= (EditText) this.findViewById(R.id.messageText);
+        cancelButton = (Button) this.findViewById(R.id.back);
+        sendButton = (Button) this.findViewById(R.id.send);
+        encrypteButton = (Button) this.findViewById(R.id.encrypt);
+        decryptButton = (Button) this.findViewById(R.id.decrypt);
+        String txt="some text to be encrypted" ;
+        String key="key phrase used for XOR-ing";
+        String frase1 = (txt+" XOR-ed to: "+(txt=xorMessage( txt, key )));
+        String encoded=base64encode( txt );
+        String frase2 = ( " is encoded to: "+encoded+" and that is decoding to: "+ (txt=base64decode( encoded )));
+        String frase3 = ( "XOR-ing back to original: "+xorMessage( txt, key ) );
+        messageText.setText(frase1+"-------"+frase2+"==========="+frase3);
     }
 
 
